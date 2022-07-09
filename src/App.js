9a67fa738dfdc8b1dc0775e50be820b0d3eedd43
anooja62 <anooja7777@gmail.com>
2022-07-09 22:28:14 +0530

    API integration fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import './App.css'
+import {originals,action} from './urls'
 import Banner from './Components/Banner/Banner'
 import RowPost from './Components/RowPost/RowPost'
 
@@ -10,7 +11,8 @@ function App() {
     <div>
       <NavBar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
     </div>
   )
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,6 @@
-// export const baseUrl ='https://api.themoviedb.org/3';
-// export const API_KEY ="973878d925cddeaf0ee72cc0e9b87422";
-// export const imageUrl ='https://image.tmdb.org/t/p/original';
+ export const baseUrl ='https://api.themoviedb.org/3';
+ export const API_KEY ="973878d925cddeaf0ee72cc0e9b87422";
+export const imageUrl ='https://image.tmdb.org/t/p/original';
 
 
-export const baseUrl= 'https://api.themoviedb.org/3';
-export const API_KEY ="ef5d168926037c1ad64d2ce5286717b6";
-export const imageUrl = 'https://image.tmdb.org/t/p/original'
+ 
